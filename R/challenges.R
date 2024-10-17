d8d6db9582b69aa3b8743596fca7f51b46fe2e5a
Alex_Nguyen_VN <tngu0478@student.monash.edu>
2024-10-17 14:21:10 +1100

    Testing RCPP

--- R/challenges.R
+++ R/challenges.R
@@ -8,9 +8,20 @@
 #' m <- matrix(seq(16),nrow=4)
 #' un(m)
 #' @export
-un <- function(m) {
-  mean(apply(m,1,mean) + apply(m,2,mean))
-}
+
+un <- Rcpp::cppFunction('double un_ccp(NumericMatrix m) {
+  int n = m.nrow();
+  double sum = 0;
+  for (int i = 0; i < n; i++) {
+    double row_mean = 0;
+    for (int j = 0; j < n; j++) {
+      row_mean += m(i, j);
+    }
+    sum += row_mean / n;
+  }
+  return sum;
+}')
+
 
 #' @title Second
 #' @description
@@ -19,29 +30,28 @@ un <- function(m) {
 #' @param vec Numerical vector with no missing values
 #' @return A numerical vector containing the longest continuous increasing subset
 #' @export
-deux <- function(vec) {
-  longest_seq <- c(vec[1])  # Longest increasing subsequence found
-  current_seq <- c(vec[1])  # Current increasing subsequence
-
-  for (i in 2:length(vec)) {
-    if (vec[i] > vec[i - 1]) {
-      # Continue current increasing subsequence
-      current_seq <- c(current_seq, vec[i])
+deux <-  Rcpp::cppFunction('IntegerVector deux_ccp(IntegerVector vec) {
+  IntegerVector longest_seq;
+  IntegerVector current_seq;
+  
+  for (int i = 0; i < vec.size(); i++) {
+    if (i == 0 || vec[i] > vec[i - 1]) {
+      current_seq.push_back(vec[i]);
     } else {
-      # Compare and reset current subsequence
-      if (length(current_seq) > length(longest_seq)) {
-        longest_seq <- current_seq
+      if (current_seq.size() > longest_seq.size()) {
+        longest_seq = current_seq;
       }
-      current_seq <- c(vec[i])  # Start a new subsequence
+      current_seq = IntegerVector();
+      current_seq.push_back(vec[i]);
     }
   }
-
-  if (length(current_seq) > length(longest_seq)) {
-    longest_seq <- current_seq
+  
+  if (current_seq.size() > longest_seq.size()) {
+    longest_seq = current_seq;
   }
   
-  return(longest_seq)
-}
+  return longest_seq;
+}')
 
 #' @title Third
 #' @description
@@ -52,6 +62,10 @@ deux <- function(vec) {
 #' @return A single numerical vector with counts of each unique element
 #'
 #' @export
-trois <- function(vec) {
-  table(vec)
-}
+trois <-  Rcpp::cppFunction('IntegerVector trois_ccp(IntegerVector vec) {
+  IntegerVector out(10);
+  for (int i = 0; i < vec.size(); i++) {
+    out[vec[i] - 1]++;
+  }
+  return out;
+}')
